34e2ef3da83ea5a8d253cddabe75c234389c5d15
thoranna <thorannabender@gmail.com>
2023-07-23 15:15:14 +0200

    loc ch

--- reader.py
+++ reader.py
@@ -4,10 +4,12 @@ substrs = [
     "Human kernel: ",
     "Combined: ",
     "Machine kernel: ",
-    "TAR: "
+    "TAR: ",
+    "SNaCK TAR: ",
+    "TAR MACHINE KERNEL: "
 ]
 
-with open("output2.txt", "r") as f:
+with open("output_1002.txt", "r") as f:
     for line in f:
         # If the line starts with any of the substrings, print it
         if any(line.startswith(substr) for substr in substrs):
